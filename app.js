997122a6d7a930094827f19859b03649dbe175d0
Weslyn Wagner <weslynwagner@gmail.com>
2018-09-04 23:19:27 -0500

    add next page rendering

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function submitButton(){
+    $('.button').click(function(event){
+        location.href = "file:///home/weslyn/Desktop/GitHub/Web_Dev/HTML_Examples/classNotes.html";
+    });
+}
+$(submitButton);
